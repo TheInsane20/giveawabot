948d3a36d3f760202090215450fa6a488ebd601e
TheInsane20 <81353223+TheInsane20@users.noreply.github.com>
2021-03-25 14:41:53 +0530

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -3,16 +3,16 @@ const config = require('../config.json');
 
 module.exports.run = async (client, message, args) => {
     let prefix = config.prefix;
-    if (message.author.bot) return;
+    if (message..bot) return;
     if(!message.content.startsWith(prefix)) return;
 
     let invite = new Discord.MessageEmbed()
     .setTitle("Invite & Support Link!")
-    .addField("Support Server", "[Click to join support Server](https://discord.gg/FyJjxz4Nep)")
+    .addField("Support Server", "[Click to join support Server](https://discord.gg/WAcxYTWs)")
     .setTitle("Bot invite Link")
     .addField("Invite Link", "[Click to invite the bot](https://discord.com/api/oauth2/authorize?client_id=820871786677010453&permissions=8&scope=bot)")
     .setTimestamp()
-    .setFooter(`Requested by ${message.author.tag}`, client.user.displayAvatarURL())
+    .setFooter(`Requested by ${message..tag}`, client.user.displayAvatarURL())
     message.channel.send(invite);
 }
 
